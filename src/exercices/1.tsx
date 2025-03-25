ee4e29dda2707e1d468e9cb1280c1115ac101709
Stephane Guery <58395456+Fanou59@users.noreply.github.com>
2025-03-25 09:24:40 +0100

    refactor: define types for Square and Board components, and update rendering logic

--- src/exercices/1.tsx
+++ src/exercices/1.tsx
@@ -1,29 +1,67 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 /* eslint-disable @typescript-eslint/prefer-ts-expect-error */
 import clsx from "clsx";
+import { ComponentPropsWithoutRef } from "react";
 
-// 🦁 Supprime ce commentaire et définis correctement les types pour ce composant
-type SquareProps = any;
+type SquareProps = {
+  isWinningSquare?: boolean;
+} & ComponentPropsWithoutRef<"button">;
 
-const Square = (props: SquareProps) => {
-  // 🦁 Remplace ça par les props définies en haut
+const Square = ({ isWinningSquare, children, ...props }: SquareProps) => {
   return (
     <button
       className={clsx("square", {
-        "winning-square": false, // 🦁 Remplace ça par la prop isWinningSquare
+        "winning-square": { isWinningSquare },
       })}
+      {...props}
     >
-      O {/* 🦁 Remplace ça par la prop children */}
+      {children}
     </button>
+  );
+};
+type SquareValue = "X" | "O" | null;
+
+const getDefaultSquares = (): SquareValue[] => [
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  "O",
+  null,
+  "X",
+];
+
+type BoardProps = {
+  squares: SquareValue[];
+  winningSquares?: number[];
+  onclick?: (index: number) => void;
+
+  //...
+};
+
+const Board = ({ squares, winningSquares, onclick }: BoardProps) => {
+  return (
+    <div className="game-board">
+      {squares.map((square, index) => (
+        <Square
+          key={index}
+          isWinningSquare={winningSquares?.includes(index)}
+          onClick={() => onclick?.(index)}
+        >
+          {square}
+        </Square>
+      ))}
+    </div>
   );
 };
 
 const Game = () => {
+  const squares = getDefaultSquares();
   return (
     <div className="game">
-      <Square isWinningSquare={true}>X</Square>
-      <Square isWinningSquare={false}>X</Square>
-      <Square isWinningSquare={true}>O</Square>
+      <Board squares={squares} />
     </div>
   );
 };
